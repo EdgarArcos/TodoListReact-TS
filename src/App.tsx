dd4156e74edc3fb1f526bd458bf82605cf3616c7
Edgar Arcos <edgararcosmartin@gmail.com>
2023-08-30 15:26:24 +0100

    Instalando setup estilos y solucionando problemas codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,6 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from 'vite.svg'
-import './App.css'
-
-function App() {
+const App = (): JSX.Element => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>todo TS+ract</h1>
   )
 }
 
